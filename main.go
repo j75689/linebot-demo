7035c900a19801940ba778160f299510779d1338
dylanhuang <j75689@gmail.com>
2018-12-12 21:16:17 +0800

    add port env

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	})
 	// This is just sample code.
 	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := http.ListenAndServe(":"+os.Getenv("APP_PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
 }
